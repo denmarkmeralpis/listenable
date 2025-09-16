1fa8b96ec22698de442510d1fcaf7b5b3b7e3238
Den Meralpis <denmarkmeralpis@gmail.com>
2025-09-16 21:17:04 +0800

    add ActiveSupport dependencies to Listenable module

--- lib/listenable.rb
+++ lib/listenable.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require "active_support"
+require "active_support/concern"
+require "active_support/notifications"
+
 require_relative "listenable/version"
+require_relative "listenable/concern"
+require_relative "listenable/railtie" if defined?(Rails)
 
 module Listenable
   class Error < StandardError; end
