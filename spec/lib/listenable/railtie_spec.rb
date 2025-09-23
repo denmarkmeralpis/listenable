b7a82228f345ee07ee7e086b5e483b30b0bd53cd
Den Meralpis <denmarkmeralpis@gmail.com>
2025-09-23 22:45:05 +0800

    Add RSpec tests for Listenable Railtie integration

---
+++ spec/lib/listenable/railtie_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "support/rails_dummy"
+
+RSpec.describe "Listenable Railtie integration" do
+  before do
+    UserListener.sync_called = nil
+    UserListener.async_called = nil
+  end
+
+  it "fires sync listener on record create" do
+    User.create!(name: "Test User")
+    expect(UserListener.sync_called).to eq(true)
+  end
+
+  it "fires async listener on record update" do
+    user = User.create!(name: "Test User")
+    user.update!(name: "Updated User")
+    sleep 0.05 # allow async listener to run
+    expect(UserListener.async_called).to eq(true)
+  end
+end
---
+++ spec/support/rails_dummy.rb
@@ -0,0 +1,58 @@
+require "active_record"
+require "rails"
+require "ostruct"
+require "concurrent"
+require "listenable/railtie"
+
+module Rails
+  def self.root
+    Pathname.new(File.expand_path("../..", __dir__))
+  end
+
+  def self.application
+    @application ||= begin
+      app = Object.new
+      def app.config
+        @config ||= OpenStruct.new
+      end
+      app
+    end
+  end
+end
+
+class OpenStruct
+  def to_prepare(&block)
+    block.call
+  end
+end
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Schema.define(version: 1) do
+  create_table :users, force: true do |t|
+    t.string :name
+  end
+end
+
+class User < ActiveRecord::Base; end
+
+class UserListener
+  include Listenable
+
+  listen :on_created
+  listen :on_updated, async: true
+
+  class << self
+    attr_accessor :sync_called, :async_called
+
+    def on_created(_)
+      self.sync_called = true
+    end
+
+    def on_updated(_)
+      self.async_called = true
+    end
+  end
+end
+
+initializer = Listenable::Railtie.initializers.find { |i| i.name == "listenable.load" }
+initializer.run(Rails.application)
