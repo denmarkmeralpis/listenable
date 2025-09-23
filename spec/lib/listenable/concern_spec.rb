66667e3361d90c1a446c968b07070fa6e7b9bbc9
Den Meralpis <denmarkmeralpis@gmail.com>
2025-09-23 22:44:59 +0800

    Add tests for Listenable to validate synchronous and asynchronous listening

---
+++ spec/lib/listenable/concern_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Listenable do
+  before { Listenable.enabled = true }
+
+  it "accepts listen without async" do
+    klass = Class.new do
+      include Listenable
+
+      listen :on_created
+    end
+
+    expect { klass }.not_to raise_error
+  end
+
+  it "accepts listen with async" do
+    klass = Class.new do
+      include Listenable
+
+      listen :on_created, :on_updated, async: true
+    end
+
+    expect { klass }.not_to raise_error
+  end
+end
